ac5c72adc676aebeca2110cd4a40fce16904a01b
misti <yallalraj1307@gmail.com>
2020-07-08 08:11:15 +0530

    [Rajeshwari] REFACTORED: functions and variables for gambler resign condition

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,65 +1,61 @@
 #!/bin/bash -x
+echo "Welcome to gambler simulation ...!"
 
-echo "Welcome To Gambling Simulation Problem ...!"
-
-echo "How many times you want to play?"
-read numberOfGames
-
-function flip()
+function toss()
 {
-        result=$((RANDOM%2 + 1))
-        if(($result==1))
-        then
-                echo "OUTPUT IS HEAD"
-                add=1;
-        elif(($result==2))
-        then
-                echo "OUTPUT IS TAIL"
-                sub=1;
-        fi
+    tossResult=$((RANDOM % 2))
 }
 
-for((i=0; i<numberOfGames; i++))
-do
-        echo "Which do you want to choose:"
-        echo "---------------"
-        echo "1.HEAD"
-        echo "2.TAIL"
-        read userInput
+echo "How much percent of stake amount you want to take ?"
+read stakeAmount
 
-        echo "Till how much percentage of the amount you want to play?"
-        read stakeAmount
+money=100
+bet=0
+winIncrement=1
+lossIncrement=1
+maxStake=200
+minStake=0
+won=1
+loss=1
 
-        headIncrement=1
-        tailIncrement=1
-        money=100
-        echo "I have $ 100 with me"
-        bet=0
-        echo "I will bet $ 1 now"
+function winLossStatus()
+{
+    if(($tossResult==1))
+    then
+        money=`expr $money + $won`
+        winIncrement=`expr $winIncrement + 1`
+    else
+        money=`expr $money - $loss`
+        lossIncrement=`expr $lossIncrement + 1`
+    fi
+    echo "Money is : $money"
+}
 
-        while(($money != 150 || $money != 0))
-        do
-                flip
-                if(($result==1))
-                then
-                        echo "Won $headIncrement time"
-                        money=`expr $money + $add`
-                        echo "money is $money"
-                        headIncrement=`expr $headIncrement + 1`
-                elif(($result==2))
-                then
-                        echo "won $tailIncrement time"
-                        money=`expr $money - $sub`
-                        echo "money is $money"
-                        tailIncrement=`expr $tailIncrement + 1`
-                fi
+function winLoss()
+{
+	while(($money != maxStake || $money != minStake))
+    do
+    	toss
+       	winLossStatus
 
-        winAmount=`expr $stakeAmount + 100`
+        if(($money==maxStake))
+        then
+        	echo "--------------"
+            echo "Won $maxStake"
+            break
+        elif(($money==minStake))
+        then
+        	echo "--------------"
+            echo "Lost $minStake"
+            break
+        fi
+
+		winAmount=`expr $stakeAmount + 100`
         lossAmount=`expr 100 - $stakeAmount`
         bet=`expr $bet + 1`
         echo "Number of bet is $bet"
 
-        if(($money == $winAmount))
+		if(($money == $winAmount))
         then
             echo "Won $ $winAmount for $stakeAmount %"
             break
@@ -68,6 +64,6 @@ do
             echo "lost $ $lossAmount for $stakeAmount %"
             break
         fi
-    done
-done
-
+	done
+}
+winLoss
