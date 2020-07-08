f00fc8a544112d0563b7ac6f7234916432482d9d
misti <yallalraj1307@gmail.com>
2020-07-08 08:40:33 +0530

    [Rajeshwari] ADDED: functions that each month winning and loss days by how many points

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,99 +1,109 @@
 #!/bin/bash -x
 
-echo "Welcome To Gambling Simulation Problem ...!"
+echo "Welcome to gambler simulation ...!"
 
-function flip()
+function toss()
 {
-	result=$((RANDOM%2 + 1))
-	if(($result==1))
-	then
-		echo "OUTPUT IS HEAD"
-    	add=1;
-	elif(($result==2))
-	then
-		echo "OUTPUT IS TAIL"
-    	sub=1;
-	fi
+    tossResult=$((RANDOM % 2))
 }
 
-echo "Which do you want to choose:"
-echo "---------------"
-echo "1.HEAD"
-echo "2.TAIL"
-read user
-
-echo "Till how much percentage of the amount you want to play?"
+echo "How much percent of stake amount you want to take ?"
 read stakeAmount
-totalDays=21
 
-for((day=1; day<totalDays; day++))
-do
-	echo "----------DAY $day -------------"
+bet=0
+winIncrement=1
+lossIncrement=1
+maxStake=200
+minStake=0
+won=1
+loss=1
+totalDays=30
 
-    headIncrement=1
-    tailIncrement=1
-    money=100
-
-   	echo "I have $ 100 with me"
-    bet=0
-    echo "I will bet $ 1 now"
-
-    while(($money != 200 || $money != 0))
-    do
-    	flip
-        if(($result==1))
-        then
-        	echo "won $headIncrement time"
-            money=`expr $money + $add`
-            echo "money is $ $money"
-            headIncrement=`expr $headIncrement + 1`
-		elif(($result==2))
-        then
-        	echo "won $tailIncrement time"
-            money=`expr $money - $sub`
-            echo "money is $ $money"
-            tailIncrement=`expr $tailIncrement + 1`
-		fi
-
-        winAmount=`expr $stakeAmount + 100`
-        lossAmount=`expr 100 - $stakeAmount`
-        bet=`expr $bet + 1`
-        echo "number of bet is $bet"
-
-       	if(($money == $winAmount))
-        then
-        	echo "won $ $winAmount for $ $stakeAmount %"
-            break
-        elif(($money == $lossAmount))
-        then
-        	echo "lost $ $lossAmount for $ $stakeAmount %"
-            break
-        fi
-	done
-
-	echo "--------------------------------"
-	echo "money for day $day is $ $money"
-	totalMoneyWon+=( "$money" )
-	echo "--------------------------------"
-done
+function winLossStatus()
+{
+    if(($tossResult==1))
+    then
+        money=`expr $money + $won`
+        winIncrement=`expr $winIncrement + 1`
+    else
+        money=`expr $money - $loss`
+        lossIncrement=`expr $lossIncrement + 1`
+    fi
+	echo "Money is : $money"
+}
 
 sum=0
-for count in ${totalMoneyWon[@]}
-do
-    sum=`expr $sum + $count`
-done
+totalBetAmount=3000
 
-echo "-----------------------------------------------"
-echo "Total money won after playing 20 days is $ $sum"
-echo "---------------------------------------------"
-echo "you bet $ 2000"
-if(($sum<2000))
-then
-    moneyLoss=`expr 2000 - $sum `
-    echo "Total money loss in 20 days is $ $moneyLoss"
-    echo "Your are in loss"
-else
-    exit
-fi
+function profitLoss()
+{
+    for count in ${totalMoneyWon[@]}
+    do
+        sum=`expr $sum + $count`
+    done
+    echo "-----------------------------------------------"
+    echo "Total money won after playing $totalDays days is $ $sum, you bet $totalBetAmount"
 
+    if(($sum<$totalBetAmount))
+    then
+        moneyLoss=`expr $totalBetAmount - $sum `
+        echo "Total money loss in $totalDays days is $ $moneyLoss, YOU ARE IN LOSS."
+    else
+        exit
+    fi
+}
+
+function winLoss()
+{
+	for((day=0; day<totalDays; day++))
+    do
+    	days=`expr $day + 1`
+        echo "----------DAY $days -------------"
+        money=100
+        while(($money != maxStake || $money != minStake))
+        do
+        	toss
+            winLossStatus
+
+            if(($money==maxStake))
+            then
+                echo "Won $maxStake"
+                break
+            elif(($money==minStake))
+            then
+                echo "Lost $minStake"
+                break
+            fi
+
+            winAmount=`expr $stakeAmount + 100`
+            lossAmount=`expr 100 - $stakeAmount`
+            bet=`expr $bet + 1`
+
+            if(($money == $winAmount))
+            then
+            	echo "Won $ $winAmount for $stakeAmount %"
+                break
+            elif(($money == $lossAmount))
+            then
+                echo "lost $ $lossAmount for $stakeAmount %"
+                break
+            fi
+		done
+        echo "money for day $days is $ $money"
+        totalMoneyWon+=( "$money" )
+
+		if(($money>100))
+       	then
+        	dayWon+=( "$days" )
+        elif(($money<100))
+        then
+        	dayLoss+=( "$days")
+       	fi
+	done
+
+	echo "Winning days are : ${dayWon[@]}"
+	echo "Winning days are : ${dayLoss[@]}"
+}
+winLoss
+profitLoss
 
